7598b4f2a0f66cd08be92b9ac1c155125d3b566c
Bohdan Dats'ko <53016307+BilyiVoron@users.noreply.github.com>
2020-04-12 19:04:07 +0300

    API for store (#47)

--- lantern/flask/flask_blueprints_args/practice/grocery_store/routes/stores.py
+++ lantern/flask/flask_blueprints_args/practice/grocery_store/routes/stores.py
@@ -1,25 +1,21 @@
 import inject
-from flask import Blueprint, request, jsonify
-
-stores_bl = Blueprint("stores", __name__)
-
-
-@stores_bl.route('/store', methods=['POST'])
-def create_store():
-    db = inject.instance('DB')
-    store_id = db.stores.add(request.json)
-    return jsonify({'store_id': store_id}), 201
+from flask import request
+from flask_restful import Resource
 
 
-@stores_bl.route('/store/<int:store_id>')
-def get_store(store_id):
-    db = inject.instance('DB')
-    store = db.stores.get_store_by_id(store_id)
-    return jsonify(store)
+class Store(Resource):
 
+    def get(self, store_id):
+        db = inject.instance('DB')
+        store = db.stores.get_store_by_id(store_id)
+        return store
 
-@stores_bl.route('/store/<int:store_id>', methods=['PUT'])
-def update_store(store_id):
-    db = inject.instance('DB')
-    db.stores.update_store_by_id(store_id, request.json)
-    return jsonify({'status': 'success'})
+    def post(self):
+        db = inject.instance('DB')
+        store_id = db.stores.add(request.json)
+        return {'store_id': store_id}, 201
+
+    def put(self, store_id):
+        db = inject.instance('DB')
+        db.stores.update_store_by_id(store_id, request.json)
+        return {'status': 'success'}
--- lantern/flask/flask_blueprints_args/practice/grocery_store/store_app.py
+++ lantern/flask/flask_blueprints_args/practice/grocery_store/store_app.py
@@ -1,9 +1,11 @@
 import inject
 from flask import Flask
+from flask_restful import Api
 
 from errors import NoSuchUserError, my_error_handler, NoSuchStoreError
 from fake_storage import FakeStorage
-from routes import users_bl, goods_bl, stores_bl
+from routes import users_bl, goods_bl
+from routes.stores import Store
 
 
 def configure(binder):
@@ -16,10 +18,12 @@ def make_app():
     inject.clear_and_configure(configure)
 
     app = Flask(__name__)
+    # API
+    api = Api(app)
+    api.add_resource(Store, "/store", "/store/<int:store_id>")
     # register blueprints and error handlers
     app.register_blueprint(users_bl)
     app.register_blueprint(goods_bl)
-    app.register_blueprint(stores_bl)
 
     app.register_error_handler(NoSuchUserError, my_error_handler)
     app.register_error_handler(NoSuchStoreError, my_error_handler)
