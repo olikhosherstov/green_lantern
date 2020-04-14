5942f50f444d72485f599264af115960b4d28896
olikhosherstov <sgut@ukr.net>
2020-04-14 18:11:56 +0300

    refactoring

--- lantern/Fill_db_scripts/script.py
+++ lantern/Fill_db_scripts/script.py
@@ -1,61 +1,75 @@
-import csv, requests, argparse
+import argparse
+import csv
 from random import randrange
 
-
-def add_users(url:str, count_user):
-    with open('data/names.csv') as fill:
-        name_read = csv.reader(fill, delimiter=",")
-        names = {int(line[0]): line[1] for line in name_read}
-    with open('data/surnames.csv') as fill:
-        reading = csv.reader(fill, delimiter=",")
-        surnames = {int(line[0]): line[1] for line in reading}
-    for i in range(count_user):
-        # Can been 17728 different combinations
-        rand_name = " ".join([names.get(randrange(1,65),1), surnames.get(randrange(1,277),1)])
-        post_user = {'name': rand_name}
-        requests.post(url, json=post_user)
-        print(rand_name)
-    print(f"Added {i} users.")
+import requests
 
 
-def add_goods(url:str, count_goods):
-    with open('data/colors.csv') as fill:
-        post_read = csv.reader(fill, delimiter=",")
-        colors = {int(line[0]): line[1] for line in post_read}
-    with open('data/clothes.csv') as fill:
-        post_read = csv.reader(fill, delimiter=",")
-        clothes = {int(line[0]): line[1] for line in post_read}
-    with open('data/size.csv') as fill:
-        post_read = csv.reader(fill, delimiter=",")
-        size = {int(line[0]): line[1] for line in post_read}
+def data_sets(data_name: str):
+    with open(f'data/{data_name}.csv') as fill:
+        data_read = csv.reader(fill, delimiter=",")
+        return {int(line[0]): line[1] for line in data_read}
+
+
+def add_users(url: str, count_user):
+    names = data_sets("names")
+    surnames = data_sets("surnames")
+    rand_name = lambda x_name, y_surname: " ".join([x_name.get(randrange(1, 65), 1),
+                                                    y_surname.get(randrange(1, 277), 1)])
+    try:
+        if requests.post(url, json={'name': rand_name(names, surnames)}).status_code == 201:
+            for i in range(count_user):
+                # Can been 17728 different combinations
+                data = rand_name(names, surnames)
+                requests.post(url, json={'name': data})
+                print(data)
+            print(f"Sended to server {i + 1} requests.")
+        else:
+            print("Invalid table.")
+    except ConnectionRefusedError:
+        print(f"Server {url} is not connected. Try another URL.")
+
+
+def add_goods(url: str, count_goods):
+    colors = data_sets("colors")
+    clothes = data_sets("clothes")
+    size = data_sets("size")
     post_goods = []
+    rand_name = lambda x, y, z: " ".join([x.get(randrange(1, 151), 1),
+                              y.get(randrange(1, 78), 1),
+                              z.get(randrange(1, 9), 1)]
+                             )
     for i in range(count_goods):
         # Can been 106002 different combinations
-        rand_name = " ".join([colors.get(randrange(1,151),1),
-                              clothes.get(randrange(1,78),1),
-                              size.get(randrange(1,9),1)]
-                             )
-        post_goods.append({'name': rand_name, 'price': randrange(1,100)})
-    print(post_goods)
-    resp = requests.post(url, json=post_goods)
-    print(f"Added {resp.json()['numbers of items created']} goods.")
+        post_goods.append({'name': rand_name(colors, clothes, size), 'price': randrange(1, 100)})
+    try:
+        resp = requests.post(url, json=post_goods)
+        if resp.status_code == 201:
+            print(f"Sended to server serial data with {resp.json()['numbers of items created']} goods.")
+        else:
+            print("Invalid table.")
+    except ConnectionRefusedError:
+        print(f"Server {url} is not connected. Try another URL.")
 
 
-def add_store(url:str, count_stores):
-    with open('data/colors.csv') as fill:
-        post_read = csv.reader(fill, delimiter=",")
-        colors = {int(line[0]): line[1] for line in post_read}
-    with open('data/animals.csv') as fill:
-        post_read = csv.reader(fill, delimiter=",")
-        animals = {int(line[0]): line[1] for line in post_read}
-    for i in range(count_stores):
-        # Can been 7500 different combinations
-        rand_name = " ".join([colors.get(randrange(1,151),1), animals.get(randrange(1,51),1)])
-        post_store = {'name': rand_name, 'location': 'Lviv', 'manager_id': 1}
-        resp = requests.post(url, json=post_store)
-        print(post_store)
-        print(resp.json)
-    print(f"Added {i} stores.")
+def add_store(url: str, count_stores):
+    colors = data_sets("colors")
+    animals = data_sets("animals")
+    rand_name = lambda x, y: " ".join([x.get(randrange(1, 151), 1), y.get(randrange(1, 51), 1)])
+    try:
+        resp = requests.post(url, json={'name': rand_name(colors, animals),
+                                        'location': 'Lviv', 'manager_id': 1})
+        if resp.status_code == 201:
+            for i in range(count_stores):
+                # Can been 7500 different combinations
+                post_store = {'name': rand_name(colors, animals), 'location': 'Lviv', 'manager_id': 1}
+                requests.post(url, json=post_store)
+                print(post_store)
+            print(f"Sended to server {i + 1} requests.")
+        else:
+            print("Invalid table or users not added.")
+    except ConnectionRefusedError:
+        print(f"Server {url} is not connected. Try another URL.")
 
 
 if __name__ == "__main__":
