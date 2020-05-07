1558cab45df121171f193a685e506a3fc5f31c5b
olikhosherstov <sgut@ukr.net>
2020-05-07 15:32:55 +0300

    docker working application beta

--- lantern/docker/practice/app.py
+++ lantern/docker/practice/app.py
@@ -1,39 +1,8 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_utils import create_database, drop_database, database_exists
 from config import Config
 from populate_data import get_users, get_goods, get_stores
-
-db = SQLAlchemy()
-
-
-class Users(db.Model):
-    __tablename__ = 'users'
-
-    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String, nullable=False)
-    email = db.Column(db.String,  nullable=False)
-    password = db.Column(db.String, nullable=False)
-
-
-class Goods(db.Model):
-    __tablename__ = 'goods'
-
-    good_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    brand = db.Column(db.String, nullable=False)
-    name = db.Column(db.String,  nullable=False)
-    price = db.Column(db.Float, nullable=False)
-
-
-class Stores(db.Model):
-    __tablename__ = 'stores'
-
-    store_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String, nullable=False)
-    city = db.Column(db.String,  nullable=False)
-    address = db.Column(db.String, nullable=False)
-    manager_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
-
+from models import db, Users, Goods, Stores
 
 app = Flask(__name__)
 app.config.from_object(Config)
--- lantern/docker/practice/config.py
+++ lantern/docker/practice/config.py
@@ -2,8 +2,9 @@ class Config:
     TEST_VALUE = "CONFIG_VALUE"
     PG_USER = "cursor"
     PG_PASSWORD = "very_secret_password"
-    PG_HOST = "172.28.1.4"
-    PG_PORT = 5433
+    PG_HOST = "db"
+    PG_PORT = 5432
     DB_NAME = "cursor_sqlalchemy_db"
     SQLALCHEMY_DATABASE_URI = f"postgresql://{PG_USER}:{PG_PASSWORD}@{PG_HOST}:{PG_PORT}/{DB_NAME}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
