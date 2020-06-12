c92f182e50f51ce72c13e7637286f72d812f2242
sgut-cur <sgut@ukr.net>
2020-06-12 23:15:10 +0300

    fixed models

--- lantern/django/django_intro/src/apps/articles/models.py
+++ lantern/django/django_intro/src/apps/articles/models.py
@@ -13,60 +13,60 @@ class Article(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    title = models.CharField(max_length=255, verbose_name='Title', db_index=True)
-    body = models.TextField(max_length=5000, verbose_name='Article body')
+    title = models.CharField(max_length=255, verbose_name='title', db_index=True)
+    body = models.TextField(max_length=5000, verbose_name='article_body')
     tags = models.ManyToManyField(to='Tag', related_name='articles', blank=True)
     author = models.ForeignKey(
         to=settings.AUTH_USER_MODEL,
         null=True,
         on_delete=models.SET_NULL,
-        related_name='articles'
+        related_name='Articles'
     )
 
 
 class Country(models.Model):
-    CountryId = models.AutoField(primary_key=True)
-    CountryName = models.CharField(max_length=50)
+    country_id = models.AutoField(primary_key=True)
+    country_name = models.CharField(max_length=50)
 
     class Meta:
         managed = False
-        dbTable = 'country'
+        dbTable = 'Country'
 
 
 class City(models.Model):
-    CityId = models.AutoField(primary_key=True)
-    CityName = models.CharField(max_length=50)
-    CountryId = models.ForeignKey('Country', models.DO_NOTHING, db_column='CountryId', blank=True, null=True)
+    city_id = models.AutoField(primary_key=True)
+    city_name = models.CharField(max_length=50)
+    country_id = models.ForeignKey('Country', models.DO_NOTHING, db_column='country_id', blank=True, null=True)
 
     class Meta:
         managed = False
-        dbTable = 'city'
+        dbTable = 'City'
 
 
 class Menu(models.Model):
-    MenuId = models.AutoField(primary_key=True)
-    MenuName = models.CharField(max_length=50)
+    menu_id = models.AutoField(primary_key=True)
+    menu_name = models.CharField(max_length=50)
 
     class Meta:
         managed = False
-        dbTable = 'menu'
+        dbTable = 'Menu'
 
 
 class Dish(models.Model):
-    DishId = models.AutoField(primary_key=True)
-    DishName = models.CharField(max_length=50)
+    dish_id = models.AutoField(primary_key=True)
+    dish_name = models.CharField(max_length=50)
     ingredients = models.TextField(blank=True, null=True)
     recipe = models.TextField(blank=True, null=True)
     weight = models.IntegerField()
 
     class Meta:
         managed = False
-        dbTable = 'dish'
+        dbTable = 'Dish'
 
 
 class RestaurantMenu(models.Model):
-    MenuId = models.ForeignKey('Menu', models.DO_NOTHING, db_column='MenuId', blank=True, null=True)
-    DishId = models.ForeignKey('Dish', models.DO_NOTHING, db_column='DishId', blank=True, null=True)
+    menu_id = models.ForeignKey('Menu', models.DO_NOTHING, db_column='menu_id', blank=True, null=True)
+    dish_id = models.ForeignKey('Dish', models.DO_NOTHING, db_column='dish_id', blank=True, null=True)
 
     class Meta:
         managed = False
@@ -74,28 +74,28 @@ class RestaurantMenu(models.Model):
 
 
 class Restaurant(models.Model):
-    RestaurantId = models.AutoField(primary_key=True)
-    RestaurantName = models.CharField(max_length=50)
-    CountryId = models.ForeignKey('Country', models.DO_NOTHING, db_column='CountryId', blank=True, null=True)
-    CityId = models.ForeignKey('City', models.DO_NOTHING, db_column='CityId', blank=True, null=True)
-    Address = models.CharField(unique=True, max_length=50)
-    MenuId = models.ForeignKey('RestaurantMenu', models.DO_NOTHING, db_column='MenuId', blank=True, null=True)
+    restaurant_id = models.AutoField(primary_key=True)
+    restaurant_name = models.CharField(max_length=50)
+    country_id = models.ForeignKey('Country', models.DO_NOTHING, db_column='country_id', blank=True, null=True)
+    city_id = models.ForeignKey('City', models.DO_NOTHING, db_column='city_id', blank=True, null=True)
+    address = models.CharField(unique=True, max_length=50)
+    menu_id = models.ForeignKey('RestaurantMenu', models.DO_NOTHING, db_column='menu_id', blank=True, null=True)
 
     class Meta:
         managed = False
-        dbTable = 'restaurant'
+        dbTable = 'Restaurant'
 
 
 class Staff(models.Model):
-    StaffId = models.AutoField(primary_key=True)
-    LastName = models.CharField(max_length=50)
-    FirstName = models.CharField(max_length=50)
-    BirthDate = models.DateField(blank=True, null=True)
-    Profession = models.CharField(max_length=25)
-    Skill = models.CharField(max_length=25, blank=True, null=True)
-    Salary = models.FloatField(blank=True, null=True)
-    RestaurantId = models.ForeignKey('Restaurant', models.DO_NOTHING, db_column='RestaurantId', blank=True, null=True)
+    staff_id = models.AutoField(primary_key=True)
+    last_name = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50)
+    birth_date = models.DateField(blank=True, null=True)
+    profession = models.CharField(max_length=25)
+    skill = models.CharField(max_length=25, blank=True, null=True)
+    salary = models.FloatField(blank=True, null=True)
+    restaurant_id = models.ForeignKey('Restaurant', models.DO_NOTHING, db_column='restaurant_id', blank=True, null=True)
 
     class Meta:
         managed = False
-        dbTable = 'staff'
+        dbTable = 'Staff'
